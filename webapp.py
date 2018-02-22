ccf5ed08d239a914ae36df60a112931bf91113ff
Avery Hogan <hogan.avery49@gmail.com>
2018-02-22 09:59:50 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -36,7 +36,7 @@ def inject_logged_in():
 
 @app.route('/')
 def home():
-    with open('posts.json', 'r') as posts_data
+    with open('posts.json', 'r') as posts_data:
         posts = json.load(posts_data)
         return render_template('home.html', posts=posts_to_html(posts))
 
@@ -44,19 +44,31 @@ def home():
 def post():
     #This function should add the new post to the JSON file of posts and then render home.html and display the posts.  
     #Every post should include the username of the poster and text of the post.
-    with open('posts.json', 'r+') as posts_data
-        posts = json.load(posts_data)
-        posts['user'] = github.request_token_params
-        posts['message'] = request.args['message']
+    username = session['user_data']['login']
+    message = request.form['message']
+    try:
+        with open('posts.json', 'r+') as posts_data:
+            posts = json.load(posts_data)
+            posts.append({"username":username, "message":message})
+            posts_data.seek(0)
+            posts_data.truncate()
+            json.dump(posts, posts_data)
+    except Exception as e:
+        print('Unable to load json data')
+        print(e)
         
 def posts_to_html(file):
-    with open('posts.json', 'r') as posts_data
-        posts = json.load(posts_data)
-        table = '<table ><tr><th>User</th><th>Post</th></tr>'
-        for value in posts:
-            table += '<tr><td>' + posts['user'] + '</td><td>' + posts['message'] + '</td></tr>'
-        table += '</table>
-        return table
+    table = Markup('<table ><tr><th>User</th><th>Post</th></tr>')
+    try:
+        with open('posts.json', 'r') as posts_data
+            posts = json.load(posts_data)
+            for value in posts:
+                table += Markup('<tr><td>' + posts['user'] + '</td><td>' + posts['message'] + '</td></tr>')
+            table += '</table>'
+    except Exception as e:
+        table = '<p>Unable to load json data</p>'
+        print(e)
+    return table
 
 #redirect to GitHub's OAuth page and confirm callback URL
 @app.route('/login')
