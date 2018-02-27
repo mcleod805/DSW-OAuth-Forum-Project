46f64c5caac1d0f3808133e292883ebcd0cd1b25
Avery Hogan <hogan.avery49@gmail.com>
2018-02-27 09:40:01 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -58,10 +58,9 @@ def post():
     return render_template('home.html', posts=posts_to_html())
         
 def posts_to_html():
-    forum_table = Markup("<table class='table table-bordered'> <tr> <th> Username </th> <th> Message </th> </tr>")
     try:
         with open('posts.json', 'r') as posts_data:
-            table = Markup('<table><tr><th>User</th><th>Post</th></tr>')
+            table = Markup('<table class='table table-bordered'><tr><th>User</th><th>Post</th></tr>')
             posts = json.load(posts_data)
             for value in posts:
                 table += Markup('<tr><td>' + value['username'] + '</td><td>' + value['message'] + '</td></tr>')
