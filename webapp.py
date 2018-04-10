7066778f5c2473cd5e601379510c43490da655a6
Avery Hogan <hogan.avery49@gmail.com>
2018-04-10 09:30:16 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -73,7 +73,7 @@ def posts_to_html():
     try:
         table = Markup("<table class='table table-bordered'><tr><th>User</th><th>Post</th><th>Delete</th></tr>")
         for value in collection.find():
-            table += Markup("<tr><td>" + value["user"] + "</td><td>" + value["msg"] + "</td><td><form action='/delete' method='POST'><button type='submit' name='delete' value=" + value['_id'] + ">Delete</button></form></td></tr>")
+            table += Markup("<tr><td>" + value["user"] + "</td><td>" + value["msg"] + "</td><td><form action='/delete' method='POST'><button type='submit' name='delete' value=" + str(value['_id']) + ">Delete</button></form></td></tr>")
         table += Markup("</table>")
     except Exception as e:
         table = Markup('<p>There was an error loading the table data</p>')
@@ -85,7 +85,7 @@ def delete():
     #delete posts
     global collection
     #collection.delete_one({"_id" : ObjectId(str(request.form['delete']))})
-    collection.delete_one({"_id" : str(request.form['delete'])})
+    collection.delete_one({"_id" : request.form['delete']})
     return render_template('home.html', posts=posts_to_html())
 
 #redirect to GitHub's OAuth page and confirm callback URL
